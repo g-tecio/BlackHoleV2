eb421a4c714c93ddc7a03175ec816b30a982f888
Yagufrix <antonio.sein@g-tec.io>
2018-10-23 10:31:12 -0500

    Second version
    Hole rotate

---
+++ BlackHole/Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public Transform Target;
+	private Vector3 zAxis = new Vector3(0,0,1);
+
+	public float jumpForce;
+
+	 Rigidbody2D rb;
+
+
+	enum PlayerState{
+		Standing, Jumping, Falling
+	}
+
+	PlayerState currentState = PlayerState.Falling;
+
+
+	
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+	
+		
+		
+	}
+
+	void Update()
+	{
+		GetInput();
+	}
+
+	void GetInput()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			if (currentState == PlayerState.Standing)
+			{
+				Jump();
+			}
+		}
+	}
+
+    public void Jump()
+	{
+		currentState = PlayerState.Jumping;
+		rb.AddForce(transform.position * jumpForce);
+	}
+	
+
+	
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		
+		currentState = PlayerState.Standing;
+	}
+
+
+
+}
