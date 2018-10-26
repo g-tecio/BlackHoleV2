4c5c4e5ca1692b20f9f833a0cace2ecf2679c34c
sirzsgt <diego.aragon@g-tec.io>
2018-10-26 13:04:42 -0500

    ScoreManager

---
+++ BlackHole/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour {
+
+    public TextMeshProUGUI currentScoreText;
+
+    int currentScore = 1;
+
+	void Start () {
+		
+	}
+	
+    public void AddScore(int score){
+        currentScore += score;
+        currentScoreText.text = currentScore.ToString();
+    }
+}
