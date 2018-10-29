d24db1a813d844e34625b4ba417286ef9fe3df5d
Yagufrix <antonio.sein@g-tec.io>
2018-10-29 10:21:17 -0600

    Trying to add score

---
+++ BlackHole/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+	public TextMeshProUGUI currentScoreText;
+	int currentScore;
+	// Use this for initialization
+	void Start () {
+		currentScore = 0;
+		SetScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddScore()
+	{
+		currentScore++;
+		SetScore();
+	}
+
+	void SetScore(){
+		currentScoreText.text = currentScore.ToString();
+	}
+}
--- BlackHole/Assets/Scripts/Player.cs
+++ BlackHole/Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour {
 
 	public Transform Target;
 
+    GameManager gameManager;
+
 	public float jumpForce;
 
 	 Rigidbody2D rb;
@@ -37,29 +39,48 @@ public class Player : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other)
 	{
         if(other.gameObject.name == "CoinL1"){
+            Debug.Log("+1");
+            gameManager.AddScore();
             Destroy(other.gameObject);
             //GameObject.Find("Obstacles").GetComponent<Obstacle>().speed = 100.0f;
             SceneManager.LoadScene("Level2");
+            
         }
 
         if(other.gameObject.name == "CoinL2"){
+            Debug.Log("+1");
             Destroy(other.gameObject);
             SceneManager.LoadScene("Level3");
+            
         }
 
         if (other.gameObject.name == "CoinL3")
         {
+            Debug.Log("+1");
             Destroy(other.gameObject);
             SceneManager.LoadScene("Level4");
+            
         }
 
         if (other.gameObject.name == "CoinL4")
         {
+            Debug.Log("+1");
             Destroy(other.gameObject);
             SceneManager.LoadScene("Level1");
+            
         }
 
     }
+
+    // void OnTriggerEnter2D(Collider2D other){
+    //     if (other.gameObject.tag == "Coin"){
+    //         Debug.Log("+1");
+    //         gameManager.AddScore();
+    //          Destroy(other.gameObject);
+    //         //GameObject.Find("Obstacles").GetComponent<Obstacle>().speed = 100.0f;
+    //         SceneManager.LoadScene("Level2");
+    //     }
+    // }
 
 
 
