039a04b2ed8f8d0917b6fb8cdb59cc093a311fbe
sirzsgt <diego.aragon@g-tec.io>
2018-11-06 10:53:30 -0600

    obstacles

--- BlackHole/Assets/Scripts/Game/DestroyObstacles.cs
+++ BlackHole/Assets/Scripts/Game/DestroyObstacles.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class DestroyObstacles : MonoBehaviour {
 
-	
-	public float shrinkSpeed = 3f;
+    Vector3 obstacle = new Vector3(1.5f, 1.5f, 0.0f);
+	public float shrinkSpeed = 0.1f;
 	// Use this for initialization
     public void Start () {
 		//transform.localScale = Vector3.one * 10f;
@@ -20,7 +20,7 @@ public class DestroyObstacles : MonoBehaviour {
 	}
 
 	public void ShrinkObstacles(){
-	transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+        transform.localScale -= obstacle * shrinkSpeed * Time.deltaTime;
 
 		if (transform.localScale.x <= .15f){
 			Destroy(gameObject);
